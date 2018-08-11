50a3f3c026cee330ec68a2559a8fed2c5ac41092
humanova <craxes_humanova@hotmail.com>
2018-08-11 19:38:28 +0300

    realtime python plotter + benchmark statistics
    used matplotlib to create a realtime plotter for neural net error, it gets the data from "errorRmseShort(percent).txt"

---
+++ statistics/XOR benchmark/run_plotter.bat
@@ -0,0 +1,1 @@
+cmd /c start pythonw src\error-plotter.py
---
+++ statistics/XOR benchmark/src/error-plotter.py
@@ -0,0 +1,71 @@
+#Emir Erbasan 08.08.18
+#github.com/humanova
+
+import os,sys
+import atexit
+import matplotlib as mpl
+import matplotlib.pyplot as plt 
+import matplotlib.animation as animation
+from matplotlib import style
+
+#enable/disable recording mode
+#enabling causes plotter not to
+#render in realtime
+RECORD_MODE = False
+
+mpl.rcParams['toolbar'] = 'None'
+style.use('dark_background')
+
+fig = plt.figure() 
+ax1 = fig.add_subplot(1,1,1)
+fig.canvas.set_window_title('Neural-Net Error Graph')
+
+#if RECORD_MODE:
+    #Writer = animation.writers['ffmpeg']
+    #writer = Writer(fps=30, metadata=dict(artist='humanova'), bitrate=1800)
+
+try:
+    os.chdir("Neural-Net-Settings")
+except:
+    print("Couldn't find 'Neural-Net-Setting' folder\n Please create one in the same directory with 'xor neural-net.exe'")
+
+done = False
+
+def refresh(i):
+    global done
+
+    graph_data = open('rmseErrorShort(percent).txt','r').read()
+    lines = graph_data.split('\n')[:-1]
+
+    if not done:
+        xs = []
+        ys = [] 
+        for line in lines:
+            if len(line) > 1:
+                x, y = line.split(' ')
+                ys.append(float(y))
+                xs.append(float(x))
+        
+        ax1.clear()
+        ax1.set_title('Neural-Net Error Graph')
+        ax1.set_xlabel('Epoch' + '(' + str(xs[-1])[:-2] + ')             last error: ' + str(ys[-1])  )
+        ax1.set_ylabel('Root Square Mean Error')
+
+        ax1.plot(xs, ys)
+
+        if xs[-1] % 100 == 0:
+            plt.savefig('Neural-Net Error Graph')
+
+        if graph_data.split('\n')[-1] == '-end-':
+            done = True   
+
+    else:
+        ani.event_source.stop()
+
+
+ani = animation.FuncAnimation(fig, refresh, interval = 20)
+
+#if RECORD_MODE:
+    #ani.save('Error Graph Animation.mp4', writer=writer)
+
+plt.show()
